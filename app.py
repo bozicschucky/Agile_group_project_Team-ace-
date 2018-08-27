e5e6a3d2813fd30ea633bfc8d8e69ce825418c02
sekito charlse <bozicslxye1@gmail.com>
2018-08-27 10:27:06 +0200

    add boilerplate file

---
+++ app.py
@@ -0,0 +1,26 @@
+class User(object):
+    """docstring for User"""
+
+    def __init__(self):
+        pass
+
+
+class Owners(User):
+    """docstring for Owners"""
+
+    def __init__(self):
+        pass
+
+
+class Managers(User):
+    """docstring for Managers"""
+
+    def __init__(self):
+        pass
+
+
+class Attendants(User):
+    """docstring for Attendants"""
+
+    def __init__(self):
+        pass
