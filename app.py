2a8c427dfebeaf666435b9df51e2d7bbb1cebf6b
Rhytah <ritanamono@gmail.com>
2018-08-27 11:49:34 +0300

    add owner to see daily sales

--- app.py
+++ app.py
@@ -40,10 +40,32 @@ class User(StockItem):
 
 
 class Owners(User):
-    """docstring for Owners"""
+    def __init__(self,ownerName, item):
+        self.ownerId=uuid.uuid1()
+        self.ownerName=ownerName
+        self.item = item
+        
+    
+    stock_balance=[]
+    items=[]
 
-    def __init__(self):
-        pass
+
+    def see_daily_sales(self):
+        owner = User('Gloria', 'posho')
+        item =StockItem('posho','500','1000','')
+        items=items.append(item)
+        daily_sales=[items]
+
+
+store_owners=Owners('Gloria', 'posho')
+store_owners.see_daily_sales()
+        print (daily_sales)
+
+
+    def see_stock_balances(self):
+        item_stock_balance ={item:stock_balance}
+        print (item_stock_balance)
+
 
 
 class Managers(User):
